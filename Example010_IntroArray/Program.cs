662d56d4238e9224f0daed033c45e9e28a6f2306
Berus Frager <gitmailru@yandex.ru>
2022-08-08 23:20:08 +0300

    Коммит файлов папки проекта Example010 IntroArray

--- Example010_IntroArray/Program.cs
+++ Example010_IntroArray/Program.cs
@@ -3,14 +3,15 @@
     int result = arg1;
     if (arg2 > result) result = arg2;
     if (arg3 > result) result = arg3;
-    return result;
+    return result; // Вывод значения, полученного в результате работы.
 }
-int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
-// array[0] = 12;
-// Console.WriteLine(array[0]);
-// int max = Max(
-//     Max(a1, b1, c1);
-//     Max(a2, b2, c2);
-//     Max(a3, b3, c3));
+int [] array = {11, 21, 31, 41, 115, 61, 17, 18, 19};
+// array[0] = 12; // Так можно присвоить значение элементу массива.
+// Console.WriteLine(array[4]); //Вывод значения элемента массива.
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
 
-// Console.WriteLine(max);
+Console.WriteLine(max);
