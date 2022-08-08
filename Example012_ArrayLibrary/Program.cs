639d9ece7a3f2fb686d2e0878fe7a3980e587bc2
Berus Frager <gitmailru@yandex.ru>
2022-08-09 00:38:50 +0300

    Коммит файлов папки проекта Example012 ArrayLibrary

---
+++ Example012_ArrayLibrary/Program.cs
@@ -0,0 +1,32 @@
+﻿void FillArray(int[] collection)    // FillArray - наименование
+                                    // метода. collection - 
+                                    // аргумент (какая-то 
+                                    // коллекция).
+{
+    int length = collection.Length;  // Получаем длину массива.
+    int index = 0;                  // Установка начального
+                                    // индекса.
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index ++;
+    }
+}
+
+void PrintArray(int[] col)  // Мтод void ничего не возвращает.
+                            // Поэтому оператор return 
+                            // не используется.
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+    }
+}
+int[] array = new int[10];  // Саздать новый (new) массив из 10 
+                            // элементов целых чисел. По умолчинию он будет
+                            // наполнен нулями.
+FillArray(array);
+PrintArray(array);
